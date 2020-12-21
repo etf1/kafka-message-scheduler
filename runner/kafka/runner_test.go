a9155122c0ba1c9f48e2aae77113379b4dee3fbb
Fatih KARAKAŞ <fakarakas@gmail.com>
2020-12-21 20:26:41 +0100

    Update runner_test.go

--- runner/kafka/runner_test.go
+++ runner/kafka/runner_test.go
@@ -197,7 +197,7 @@ loop:
 	// failovers succession of the runners, should not impact the number of messages published in the target topic
 	// the optimal result will be len(result) == 100, but sometimes we have more, duplicate messages in very corny cases
 	// TODO: investiguate the duplicated messages in this resilience test
-	if len(result) >= 100 {
+	if len(result) < 100 {
 		t.Fatalf("unexpected result length: %v", len(result))
 	}
 }
