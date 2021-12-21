57ee6f82178e96da65cd496dd0a42d94648ee691
fkarakas <fkarakas.malt@tf1.fr>
2021-12-21 18:36:56 +0100

    try fix tests

--- scripts/check_gotest.sh
+++ scripts/check_gotest.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 
 readonly TESTS_RESULT=$1
-readonly RUN_COUNT=$(grep "RUN" "${TESTS_RESULT}" | wc -l | awk '{print $1}')
-readonly FAIL_COUNT=$(grep "FAIL" "${TESTS_RESULT}" | wc -l | awk '{print $1}')
+# golang tests execution detection via the string "=== RUN"
+readonly RUN_COUNT=$(grep "=== RUN" "${TESTS_RESULT}" | wc -l | awk '{print $1}')
+# golang tests failure detection via the string "--- FAIL"
+readonly FAIL_COUNT=$(grep "--- FAIL" "${TESTS_RESULT}" | wc -l | awk '{print $1}')
 
 if [ "${RUN_COUNT}" -gt 0 ] && [ "${FAIL_COUNT}" -eq 0 ]; then
     echo "Test Passed !"
