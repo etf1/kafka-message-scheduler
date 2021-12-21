e33e9d41e14d1381b066614e4196bcaab74d2646
fkarakas <fkarakas.malt@tf1.fr>
2021-12-21 18:11:56 +0100

    try fix tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,14 +5,8 @@ set -xeuo pipefail
 function wait_for_kafka(){
     local server="$1"
 
-    for i in {1..100}
-    do
-        echo "Waiting for kafka cluster to be ready ..."
-        # kafkacat has 5s timeout
-        kafkacat -b "${server}" -L
-        echo "kafkacat result $?"
-        kafkacat -b "${server}" -L > /dev/null 2>&1 && break
-    done
+    echo "Waiting for kafka cluster to be ready ..."
+    kcat -m 120 -b "${server}" -L > /dev/null 2>&1 && break
 }
 
 wait_for_kafka "kafka:29092"
