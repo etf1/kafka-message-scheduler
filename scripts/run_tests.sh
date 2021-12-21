28374d6b18e5df3753b12fefd03cc360c443a6c8
fkarakas <fkarakas.malt@tf1.fr>
2021-12-21 18:15:53 +0100

    try fix tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 function wait_for_kafka(){
     local server="$1"
 
     echo "Waiting for kafka cluster to be ready ..."
-    kcat -m 120 -b "${server}" -L > /dev/null 2>&1 && break
+    kcat -m 120 -b "${server}" -L
 }
 
 wait_for_kafka "kafka:29092"
